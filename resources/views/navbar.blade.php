2ae7d4a410af979a80256592109b13965fa5c928
angreen1623 <andreopaim@gmail.com>
2024-07-11 14:48:55 -0300

    adicionando Tailwind

---
+++ resources/views/navbar.blade.php
@@ -0,0 +1,29 @@
+@vite('resources/css/app.css')
+
+<nav>
+    <div>
+
+        <div>
+            <img src="{{ asset('images/logo.png') }}" alt="Logo do TrocaTeca" width="120" class="w-50">
+        </div>
+        <div>
+
+        </div>
+        <div>
+
+        </div>
+
+    </div>
+    <div>
+
+        <div class="flex-1">
+            <svg viewBox="0 0 24 24" fill="none" width="20" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 6H20M4 12H20M4 18H20" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
+            <li><a href="">Filtros</a></li>
+        </div>
+        <li><a href="">Meus Artigos</a></li>
+        <li><a href="">Meus Acordos</a></li>
+        <li><a href="">Mensagens e Propostas</a></li>
+        <li><a href="">Quem Somos</a></li>
+
+    </div>
+</nav>
