b57d245e8bdd54a9caaf41909a81bcab3a16bb13
angreen1623 <andreopaim@gmail.com>
2024-07-12 18:17:11 -0300

    detalhe - navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -21,10 +21,7 @@
     </div>
     <div class="flex place-content-between w-full p-2 border-b border-black">
 
-        <div class="flex">
-            <svg viewBox="0 0 24 24" fill="none" class="mr-1" width="20" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M4 6H20M4 12H20M4 18H20" stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path> </g></svg>
-            <ul><a href="">Filtros</a></ul>
-        </div>
+        <ul>Filtros</ul>
         <ul><a href="">Meus Artigos</a></ul>
         <ul><a href="">Meus Acordos</a></ul>
         <ul><a href="">Mensagens e Propostas</a></ul>
