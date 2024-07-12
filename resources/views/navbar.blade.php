64519e5e0ae5947bcebe71a7dd0729df193dd602
angreen1623 <andreopaim@gmail.com>
2024-07-12 17:48:02 -0300

    navbar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -1,14 +1,18 @@
 @vite('resources/css/app.css')
 
 <nav class="w-full fixed">
-    <div class="flex w-full py-2.5 px-5 bg-sky-200 align-item-center place-content-between items-center">
+    <div class="flex w-full py-2.5 px-7 bg-bluett align-item-center place-content-between items-center">
 
         <div>
             <img src="{{ asset('image/logo.png') }}" alt="Logo do TrocaTeca" width="120" class="w-50">
         </div>
-        <div class="w-">
-            <label for="pesquisa"></label>
-            <input type="text" id="pesquisa" class="w-">
+        <div class="w-72">
+            <input type="text" id="pesquisa" class="h-10 w-full rounded-full">
+            <span class="absolute">
+                <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 cursor-pointer" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" />
+                </svg>
+            </span>
         </div>
         <div>
             <p class=""> <a href="" class="font-bold">Logar</a> ou <a href="" class="font-bold">cadastrar-se</a></p>
