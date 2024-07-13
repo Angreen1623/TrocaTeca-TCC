0591f2a90936c24471b81214b52025568459cfce
angreen1623 <andreopaim@gmail.com>
2024-07-13 18:10:00 -0300

    diminuir mais

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,7 +1,7 @@
 <div id="sidebar" class="bg-bluett fixed overflow-hiden z-50 translate-x-[-100%] duration-300 max-w-96 w-full h-screen">
 
     <span onclick="sidebartt()" class="absolute cursor-pointer right-0">
-        <img src="{{ asset('image/mais.svg') }}" alt="" width="50" class="rotate-45">
+        <img src="{{ asset('image/mais.svg') }}" alt="" width="40" class="rotate-45">
     </span>
     
 </div>
