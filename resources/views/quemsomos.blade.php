b6c06ed38c68286567bf2bbd4b90faa8aabb33ed
Bruno-Aparecido-Rodrigues <aparecidobruno749@gmail.com>
2024-07-13 15:44:46 -0300

    meix

--- resources/views/quemsomos.blade.php
+++ resources/views/quemsomos.blade.php
@@ -15,19 +15,13 @@
             <div class="md:flex">
                 <nav class="flex flex-wrap -my-2 max-w-xl content-center">
 
-                    <h2 class="text-4xl drop-shadow-tt mb-2 text-left text-white sm:text-6xl" style="font-family: 'Fredoka'">O que é o TrocaTeca?</h2>
+                    <h2 class="text-4xl drop-shadow-tt mb-2 text-left text-white sm:text-6xl font-fredokatt">O que é o TrocaTeca?</h2>
                     <p class="mt-2 text-slate-500 text-left">O TrocaTeca é um ambiente que visa a intermediação entre alguém que precisa se desfazer de um artigo infantil usado e alguém que precisa obter um artigo infantil. Afinal, o que não serve mais para você, pode servir para alguém, e o que não serve mais para alguém, pode servir para você!</p>
                 </nav>
 
                 <div class="w-full flex justify-end mt-4">
                     <img src="{{ asset('image/logo.png') }}" alt="Logo do TrocaTeca" width="400">
                 </div>
-        <nav class="flex flex-wrap -my-2 max-w-xl content-center">
-            
-        <h2 class="text-3xl mb-2 text-left text-white sm:text-6xl font-fredokatt">O que é TrocaTeca?</h2>    
-        <p class="mt-2 text-slate-500 text-left font-poppinstt">O TrocaTeca é um ambiente  que visa a intermediação entre alguém que precisa se desfazer de um artigo infantil usado e alguém que precisa obter um artigo infantil. Afinal, o que não serve mais para você, pode servir para alguém, e o que não serve mais para alguém, pode servir para você!</p>
-        </nav>
-
             </div>
         </div>
     </div>
