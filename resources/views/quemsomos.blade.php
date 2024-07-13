6994ed86fa4b7a6d141a61263fb2b48775183201
IgorNogueiraPessoa <igornnpessoa@gmail.com>
2024-07-13 14:55:21 -0300

    QuemSomos

--- resources/views/quemsomos.blade.php
+++ resources/views/quemsomos.blade.php
@@ -18,13 +18,14 @@
      <div class="md:flex"> 
 
 
-        <nav class="flex flex-wrap -mx-5 -my-2 max-w-xl">
-        <h2 class="">O que é TrocaTeca</h2>    
-        <p class="mt-2 text-slate-500 text-left self-center">O TrocaTeca é um ambiente  que visa a intermediação entre alguém que precisa se desfazer de um artigo infantil usado e alguém que precisa obter um artigo infantil. Afinal, o que não serve mais para você, pode servir para alguém, e o que não serve mais para alguém, pode servir para você!</p>
+        <nav class="flex flex-wrap -my-2 max-w-xl content-center">
+            
+        <h2 class="text-3xl mb-2 text-left text-white sm:text-6xl" style="font-family: 'Fredoka'">O que é TrocaTeca?</h2>    
+        <p class="mt-2 text-slate-500 text-left">O TrocaTeca é um ambiente  que visa a intermediação entre alguém que precisa se desfazer de um artigo infantil usado e alguém que precisa obter um artigo infantil. Afinal, o que não serve mais para você, pode servir para alguém, e o que não serve mais para alguém, pode servir para você!</p>
         </nav>
 
-        <div class="w-full flex justify-end mt-4">
-            <img src="{{ asset('image/logo.png') }}" alt="Logo do TrocaTeca" width="398">
+        <div class="w-full flex justify-end mt-4"> 
+            <img src="{{ asset('image/logo.png') }}" alt="Logo do TrocaTeca" width="400">
         </div>
 
     </div>
