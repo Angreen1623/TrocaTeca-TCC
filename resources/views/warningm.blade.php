0a9fd498d58e2f0394971888e142885a71dfe035
Bruno-Aparecido-Rodrigues <aparecidobruno749@gmail.com>
2024-07-13 14:16:07 -0300

    warning modal

--- resources/views/warningm.blade.php
+++ resources/views/warningm.blade.php
@@ -1,19 +1,27 @@
 @vite('resources/css/app.css')
 
-<div class="rounded-lg bg-bluett p-8 shadow-2xl">
-  <h2 class="text-lg font-bold">Are you sure you want to do that?</h2>
+<div class="fixed inset-0 flex items-center justify-center p-4 sm:p-8">
+  <div class="w-full max-w-md rounded-3xl bg-bluett p-6 sm:p-8 shadow-2xl">
+    <h1 class="text-2xl sm:text-4xl font-bold text-center text-white">AVISO!</h1>
 
-  <p class="mt-2 text-sm text-graytt">
-    Doing that could have cause some issues elsewhere, are you 100% sure it's OK?
-  </p>
+    <p class="mt-2 text-sm sm:text-base text-black text-center">
+      Ao confirmar a inativação da conta você perderá todo o seu acesso a ela.
+      Todas as propostas realizadas, artigos publicados e outros registros estarão
+      indisponíveis para visualização por qualquer outra pessoa.
+    </p>
 
-  <div class="mt-4 flex gap-2">
-  <button class="inline-flex items-center px-4 py-2 bg-white hover:bg-graytt-light text-black text-sm font-medium rounded-2xl">
-Cancelar
-  </button>
-  <button class="inline-flex items-center px-4 py-2 bg-redtt hover:bg-redtt-dark text-white text-sm font-medium rounded-2xl">
-	Inativar
-  </button>
+    <div class="mt-4 flex items-center  gap-2">
+      <input type="checkbox" id="confirm" name="confirm" class="mr-2">
+      <label for="confirm" class="text-sm sm:text-base text-black">Estou ciente disso</label>
+    </div>
+
+    <div class="mt-4 flex gap-16 justify-center">
+      <button class="inline-flex items-center px-4 py-2 bg-white hover:bg-graytt-light text-black text-sm font-medium rounded-2xl">
+        Cancelar
+      </button>
+      <button class="inline-flex items-center px-4 py-2 bg-redtt hover:bg-redtt-dark text-white text-sm font-medium rounded-2xl">
+        Inativar
+      </button>
+    </div>
   </div>
 </div>
-
