bb83c2291f36e466d814f081334695117c9a529b
IgorNogueiraPessoa <igornnpessoa@gmail.com>
2024-07-14 17:29:56 -0300

    Pagina Inicial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -17,18 +17,18 @@
         <div class="bg-greentt-light">
     <div class="max-w-screen-xl px-4 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
         <div class="lg:flex lg:justify-between lg:items-center flex flex-col-reverse lg:flex-row">
-            <nav class="flex flex-col justify-center space-y-4 max-w-3xl mt-4 lg:mt-0 lg:order-1">
-                <h2 class="text-5xl drop-shadow-tt text-left text-white sm:text-6xl" style="font-family: 'Fredoka'; white-space: nowrap;">
+            <nav class="mb-7 flex flex-col justify-center space-y-4 max-w-3xl lg:mt-0 lg:order-1">
+                <h2 class="text-3xl drop-shadow-tt text-left text-white sm:text-6xl" style="font-family: 'Fredoka'; white-space: nowrap;">
                     Bem vindo ao TrocaTeca
                 </h2>
-                <p class="mt-1 text-black text-left lg:mt-2">Infância com sustentabilidade e reusabilidade</p>
-                <button class="mb- w-36 text-center items-center px-4 py-2 shadow-tt bg-pinktt hover:bg-pinktt-dark text-white text-sm font-medium rounded-2xl mt-4 lg:mt-0 mr-5 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300">
+                <p class="text-black text-left lg:mt-2">Infância com sustentabilidade e reusabilidade</p>
+                <button class="w-36 text-center items-center px-4 py-2 shadow-tt bg-pinktt hover:bg-pinktt-dark text-white text-sm font-medium rounded-2xl mt-4 lg:mt-0 mr-5 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300">
                     <a href="/about">Quem somos</a>
                 </button>
             </nav>
 
             <div class="flex justify-center lg:justify-end lg:order-2 mb-4 lg:mb-0">
-                <img src="{{ asset('image/logo-full.png') }}" alt="Logo do TrocaTeca" width="550" class="py-12 lg:py-24 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300">
+                <img src="{{ asset('image/logo-full.png') }}" alt="Logo do TrocaTeca" width="550" class="py-10 lg:py-24 transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300">
             </div>
         </div>
     </div>
