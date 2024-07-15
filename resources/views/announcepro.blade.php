7dcaa4cdedd459b836f1fa74c8de905280057b7d
Bruno-Aparecido-Rodrigues <aparecidobruno749@gmail.com>
2024-07-15 17:05:36 -0300

    responsividade

--- resources/views/announcepro.blade.php
+++ resources/views/announcepro.blade.php
@@ -18,10 +18,10 @@
             <h1 class=" text-3xl lg:text-4xl font-bold text-center text-white font-fredokatt drop-shadow-tt mt-5">Anuncie um Novo Artigo</h1> <!--Título-->
             <!--input de imagem-->
             <div class="flex flex-col lg:flex-row lg:items-center lg:place-content-center mb-5">
-                <div class="mr-8">
+                <div class="sm:mr-8">
                     <div class="mt-5 lg:mt-0">
                         <label for="screenshot">
-                            <div class="bg-white w-full lg:h-80 lg:w-80 t-2 rounded-xl border border-graytt-light shadow-tt flex flex-col flex-wrap justify-center items-center">
+                            <div class="bg-white w-full lg:h-80 lg:w-80 rounded-xl border border-graytt-light shadow-tt flex flex-col flex-wrap justify-center items-center">
                                 <div class="mt-2 mb-2 flex flex-col justify-center items-center transition ease-in-out delay-100  hover:-translate-y-1 hover:scale-[1.05] duration-300">
                                     <img src="{{asset('image/mais.svg')}}" alt="" width="100">
                                     <p class="text-graytt text-xs text-center mt-2">Foto Principal do Artigo (Obrigatório)</p>
@@ -36,7 +36,7 @@
                             <div>
                                 <input type="file" name="capdenun" id="screenshot" class="hidden">
                                 <label for="screenshot">
-                                <div class="mt-3 bg-white p-3  rounded-xl border border-graytt-light shadow-tt flex flex-col justify-center items-center">
+                                <div class="mt-3 bg-white p-3 sm:mr-3 rounded-xl border border-graytt-light shadow-tt flex flex-col justify-center items-center">
                                     <div class="flex flex-col justify-center items-center transition ease-in-out delay-100  hover:-translate-y-1 hover:scale-[1.05] duration-300">
                                         <img src="{{asset('image/mais.svg')}}" alt="" width="30">
                                     </div>
@@ -46,7 +46,7 @@
                             <div>
                                 <input type="file" name="capdenun" id="screenshot" class="hidden">
                                 <label for="screenshot">
-                                <div class="mt-3 bg-white p-3  rounded-xl border border-graytt-light shadow-tt flex flex-col justify-center items-center">
+                                <div class="mt-3 bg-white p-3 sm:mr-3 rounded-xl border border-graytt-light shadow-tt flex flex-col justify-center items-center">
                                     <div class="flex flex-col justify-center items-center transition ease-in-out delay-100  hover:-translate-y-1 hover:scale-[1.05] duration-300">
                                         <img src="{{asset('image/mais.svg')}}" alt="" width="30">
                                     </div>
@@ -56,7 +56,7 @@
                             <div>
                                 <input type="file" name="capdenun" id="screenshot" class="hidden">
                                 <label for="screenshot">
-                                <div class="mt-3 bg-white p-3  rounded-xl border border-graytt-light shadow-tt flex flex-col justify-center items-center">
+                                <div class="mt-3 bg-white p-3 sm:mr-3 rounded-xl border border-graytt-light shadow-tt flex flex-col justify-center items-center">
                                     <div class="flex flex-col justify-center items-center transition ease-in-out delay-100  hover:-translate-y-1 hover:scale-[1.05] duration-300">
                                         <img src="{{asset('image/mais.svg')}}" alt="" width="30">
                                     </div>
@@ -66,7 +66,7 @@
                             <div>
                                 <input type="file" name="capdenun" id="screenshot" class="hidden">
                                 <label for="screenshot">
-                                <div class="mt-3 bg-white p-3  rounded-xl border border-graytt-light shadow-tt flex flex-col justify-center items-center">
+                                <div class="mt-3 bg-white p-3 rounded-xl border border-graytt-light shadow-tt flex flex-col justify-center items-center">
                                     <div class="flex flex-col justify-center items-center transition ease-in-out delay-100  hover:-translate-y-1 hover:scale-[1.05] duration-300">
                                         <img src="{{asset('image/mais.svg')}}" alt="" width="30">
                                     </div>
@@ -128,7 +128,7 @@
                         <input type="text" name="uso_art" id="uso_art" autocomplete="organization" class="shadow-tt block w-36 rounded-xl border-0 px-3.5 py-2 shadow-lg ring-1 border border-graytt ring-inset ring-graytt placeholder:text-graytt-dark focus:ring-2 focus:ring-inset lg:text-lg lg:leading-6">
                     </div>
                     <!--botões de confirmação-->
-                    <div class="flex flex-col lg:flex-row mt-4 mr-10 ml-10 gap-3 w-full justify-end">
+                    <div class="flex flex-col lg:flex-row mt-4 sm:mr-0 mr-10 w-full justify-end">
                         <button class="mr-3 inline-flex items-center p-2 justify-center w-26 lg:w-auto shadow-tt bg-pinktt hover:bg-pinktt-dark text-white text-lg font-medium rounded-2xl transition ease-in-out delay-100  hover:-translate-y-1 hover:scale-110 duration-300">
                             Criar anúncio do artigo
                         </button>
