725e640c0577c728caef4dac7f5c6144a864075f
Bruno-Aparecido-Rodrigues <aparecidobruno749@gmail.com>
2024-07-17 15:59:21 -0300

    shadow

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,4 +1,4 @@
-<div id="sidebar" class="bg-white fixed overflow-hidden z-[51] translate-x-[-150%] duration-300 max-w-96 w-full h-screen">
+<div id="sidebar" class="drop-shadow-2xl bg-white fixed overflow-hidden z-[51] translate-x-[-150%] duration-300 max-w-96 w-full h-screen">
     <div class="bg-bluett border-b border-black w-full h-20 flex place-content-center items-center">
         <p class="h-fit text-white font-fredokatt drop-shadow-tt text-3xl">Filtros</p>
         <span onclick="sidebartt()" class="absolute cursor-pointer right-5">
