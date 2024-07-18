1769f5b70e198c1ef9f2255199a6e5adc4d63b45
Bruno-Aparecido-Rodrigues <aparecidobruno749@gmail.com>
2024-07-18 14:23:09 -0300

    sidebar responsiva

--- resources/views/sidebar.blade.php
+++ resources/views/sidebar.blade.php
@@ -1,9 +1,11 @@
-<div id="sidebar" class="drop-shadow-2xl bg-white fixed overflow-hidden z-[51] translate-x-[-150%] duration-300 max-w-96 w-full h-screen">
-    <div class="bg-bluett border-b border-black w-full h-20 flex place-content-center items-center">
-        <p class="h-fit text-white font-fredokatt drop-shadow-tt text-3xl">Filtros</p>
-        <span onclick="sidebartt()" class="absolute cursor-pointer right-5">
-            <img src="{{ asset('image/mais.svg') }}" alt="" width="30" class="rotate-45">
-        </span>
-    </div>    
-            <P>butão</P>
+<div id="sidebar" class="fixed z-[60] w-screen h-screen left-0 top-0 translate-x-[-150%] bg-shadowtt">
+    <div class="drop-shadow-2xl bg-white absolute overflow-hidden duration-300 max-w-96 w-full h-screen">
+        <div class="bg-bluett border-b border-black w-full h-20 flex place-content-center items-center">
+            <p class="h-fit text-white font-fredokatt drop-shadow-tt text-3xl">Filtros</p>
+            <span onclick="sidebartt()" class="absolute cursor-pointer right-5">
+                <img src="{{ asset('image/mais.svg') }}" alt="" width="30" class="rotate-45">
+            </span>
+        </div>
+        <P>butão</P>
     </div>
+</div>
