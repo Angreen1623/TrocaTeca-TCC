3bade5ef6d5f949b4c27acc677d596c92e38b0bd
Bruno-Aparecido-Rodrigues <aparecidobruno749@gmail.com>
2024-07-18 15:57:40 -0300

    ..

--- resources/views/chat.blade.php
+++ resources/views/chat.blade.php
@@ -35,8 +35,15 @@
 
                 </div>
 
-                <div class="h-10 w-10 flex cursor-pointer">
+                <div class="h-10 w-10 flex cursor-pointer relative" onclick="menuflutuante()">
                     <svg fill="#000000" viewBox="0 0 32 32" enable-background="new 0 0 32 32" id="Glyph" version="1.1" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M13,16c0,1.654,1.346,3,3,3s3-1.346,3-3s-1.346-3-3-3S13,14.346,13,16z" id="XMLID_294_"></path><path d="M13,26c0,1.654,1.346,3,3,3s3-1.346,3-3s-1.346-3-3-3S13,24.346,13,26z" id="XMLID_295_"></path><path d="M13,6c0,1.654,1.346,3,3,3s3-1.346,3-3s-1.346-3-3-3S13,4.346,13,6z" id="XMLID_297_"></path></g></svg>
+                    <div id="menuflu" class="flex flex-col absolute hidden justify-center bg-bluett w-52 px-3 py-2 bottom-[-180px] left-[-190px] border-black border rounded-b-lg rounded-l-lg">
+                    <button id="openGiveupButton" type="button" class="mb-3 p-3 bg-white border-black border rounded-xl hover:bg-graytt-light text-black text-md font-medium rounded-2xl">
+                    Desistir da troca
+                    </button>
+                        <button type="button" class="bg-white p-3 border-black border rounded-xl hover:bg-graytt-light text-black text-md font-medium rounded-2xl">
+                            Denunciar conversa</button>
+                    </div>
                 </div>
 
             </div>
@@ -74,7 +81,11 @@
         </div>
 
     </div>
+
+    @include ('giveuptrade')
+
     <script lang="Javascript">
+        //footer do chat de conversa
         var pt_footer = document.getElementById("pt-footer");
         var footer = document.getElementById('footer');
 
@@ -82,11 +93,37 @@
         
         pt_footer.style.paddingTop = footer.offsetHeight+"px";
 
-
         function screenResize(){
             pt_footer.style.paddingTop = footer.offsetHeight+"px";
         }
 
+        //menu flutuante
+        function menuflutuante(){
+            var menuflu = document.getElementById("menuflu");
+
+
+            menuflu.classList.toggle('hidden');
+        }
+
+        function menuflutuante() {
+            document.getElementById('menuflu').classList.toggle('hidden');
+        }
+
+//modal de desistir da troca
+document.addEventListener('DOMContentLoaded', function() {
+    const openGiveupButton = document.getElementById('openGiveupButton');
+    const giveupModal = document.getElementById('giveupModal');
+
+    openGiveupButton.addEventListener('click', () => {
+        giveupModal.classList.remove('hidden'); // Mostra o modal
+        document.body.classList.add('overflow-hidden'); // Impede o scroll da página de fundo
+    });
+
+    document.getElementById('cancelGiveupButton').addEventListener('click', () => {
+        giveupModal.classList.add('hidden'); // Esconde o modal ao cancelar
+        document.body.classList.remove('overflow-hidden'); // Remove a restrição de scroll
+    });
+});
     </script>
 </body>
 
