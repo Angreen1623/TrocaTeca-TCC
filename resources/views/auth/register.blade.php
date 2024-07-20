4c8c354caedc95d55bc317a6bb365059ee9fbdd9
angreen1623 <andreopaim@gmail.com>
2024-07-20 11:58:45 -0300

    responsividade no cadastro

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -12,14 +12,15 @@
 
 <body>
     <!--Modal de cadastrar de conta-->
-    <div class="w-screen h-screen z-[45] absolute left-0 top-0 bg-backgtt bg-repeat bg-[length:870px_654px] bg-[url('/public/image/bg-icons.png')] bg-auto">
-        <div class="fixed z-50 inset-0 flex items-center justify-center p-4 sm:p-8">
+    <div class="w-screen h-screen z-[45] relative left-0 top-0 bg-backgtt bg-repeat bg-[length:870px_654px] bg-[url('/public/image/bg-icons.png')] bg-auto">
+        <div class="relative z-50 inset-0 flex items-center justify-center p-4 sm:p-8">
             <div class="w-full max-w-5xl rounded-3xl bg-bluett py-6 sm:py-8 px-16 shadow-2xl items-center drop-shadow-tt border-2 border-graytt-light">
-                <div class="flex">
-                    <div class="w-1/2 pr-10 flex flex-col justify-between h-96">
+            <form method="POST" action="{{ route('register') }}">
+            @csrf
+                <div class="flex flex-col md:flex-row"> 
+                    <div class="w-full md:w-1/2 md:pr-10 flex flex-col justify-between h-96">
                         <h1 class="text-3xl sm:text-4xl md:text-5xl font-bold text-white font-fredokatt drop-shadow-tt h-16 flex items-center" style="font-family: 'Fredoka';">Cadastrar</h1> <!--Título-->
-                        <form method="POST" action="{{ route('register') }}">
-                            @csrf
+                        
 
                             <div>
                                 <label for="name" class="text-white">Nome:</label>
@@ -54,7 +55,7 @@
                             </div>
                     </div>
 
-                    <div class="w-1/2 pl-5 border-l-2 border-white flex flex-col justify-between">
+                    <div class="w-full md:w-1/2 md:pl-5 md:border-l-2 border-white mt-3 md:mt-0 flex flex-col justify-between h-96">
                         <div>
                             <label for="estado" class="text-white">Estado:</label>
                             <select id="estado" name="estado" class="custom-select shadow-tt max-w-36 block w-full rounded-xl border border-graytt-light px-3.5 py-2 shadow-sm ring-1 border-graytt ring-inset ring-graytt placeholder:text-graytt-dark focus:ring-2 focus:ring-inset sm:text-sm sm:leading-6">
@@ -97,20 +98,21 @@
                     </div>
                 </div>
                 <div class="flex">
-                    <div class="w-1/2 flex items-center">
+                    <div class="w-full md:w-1/2 flex items-center">
                         <a class="underline-animation text-sm text-graytt-dark hover:text-graytt-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
                             {{ __('Already registered?') }}
                         </a>
                     </div>
-                    <div class="w-1/2 pl-5 border-l-2 border-white">
+                    <div class="w-full md:w-1/2 md:pl-5 md:border-l-2 border-white">
                         <div class="flex w-full justify-end items-end">
                             <button type="submit" class="inline-flex items-center justify-center w-full sm:w-auto px-4 py-2 bg-pinktt shadow-tt hover:bg-pinktt-dark text-white text-lg font-medium rounded-2xl transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-110 duration-300">
                                 Cadastrar
                             </button>
                         </div>
-                        </form>
+                        
                     </div>
                 </div>
+            </form>
             </div>
         </div>
     </div>
