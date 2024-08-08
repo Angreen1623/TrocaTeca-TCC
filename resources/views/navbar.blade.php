ae28581ae855f127b08163ca56dc67f53e95db19
Bruno-Aparecido-Rodrigues <aparecidobruno749@gmail.com>
2024-08-08 19:24:24 -0300

    colocando o sistema login na sidebar

--- resources/views/navbar.blade.php
+++ resources/views/navbar.blade.php
@@ -35,8 +35,30 @@
         <p class="underline-animation">Mensagens e Propostas</ul></a>
         <a href="/about"><ul class="w-full border-b border-graytt-dark flex flex-row py-5 items-center hover:bg-gray-200"><img src="{{ asset('image/T.svg') }}" class="w-8 h-8 mr-3">
         <p class="underline-animation">Quem Somos</p></ul></a>
+                <!-- Exibir apenas para usuários não autenticados -->
+                @guest
         <a href="/register"><ul class="w-full border-b border-graytt-dark flex flex-row py-5 items-center hover:bg-gray-200"><svg class="w-8 h-8 mr-3" fill="#000000" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" class="icon"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M858.5 763.6a374 374 0 0 0-80.6-119.5 375.63 375.63 0 0 0-119.5-80.6c-.4-.2-.8-.3-1.2-.5C719.5 518 760 444.7 760 362c0-137-111-248-248-248S264 225 264 362c0 82.7 40.5 156 102.8 201.1-.4.2-.8.3-1.2.5-44.8 18.9-85 46-119.5 80.6a375.63 375.63 0 0 0-80.6 119.5A371.7 371.7 0 0 0 136 901.8a8 8 0 0 0 8 8.2h60c4.4 0 7.9-3.5 8-7.8 2-77.2 33-149.5 87.8-204.3 56.7-56.7 132-87.9 212.2-87.9s155.5 31.2 212.2 87.9C779 752.7 810 825 812 902.2c.1 4.4 3.6 7.8 8 7.8h60a8 8 0 0 0 8-8.2c-1-47.8-10.9-94.3-29.5-138.2zM512 534c-45.9 0-89.1-17.9-121.6-50.4S340 407.9 340 362c0-45.9 17.9-89.1 50.4-121.6S466.1 190 512 190s89.1 17.9 121.6 50.4S684 316.1 684 362c0 45.9-17.9 89.1-50.4 121.6S557.9 534 512 534z"></path> </g></svg>
         <p class="underline-animation">Logar-se ou Cadastrar-se</p></ul></a>
+        @endguest
+        <!-- Exibir somente para usuários autenticados -->
+        @auth
+        <button class="group focus:outline-none border-b border-graytt-dark">
+        <div class="flex flex-row w-fit items-center mt-5 mb-3"><svg class="w-8 h-8 mr-3" fill="#000000" viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" class="icon"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path d="M858.5 763.6a374 374 0 0 0-80.6-119.5 375.63 375.63 0 0 0-119.5-80.6c-.4-.2-.8-.3-1.2-.5C719.5 518 760 444.7 760 362c0-137-111-248-248-248S264 225 264 362c0 82.7 40.5 156 102.8 201.1-.4.2-.8.3-1.2.5-44.8 18.9-85 46-119.5 80.6a375.63 375.63 0 0 0-80.6 119.5A371.7 371.7 0 0 0 136 901.8a8 8 0 0 0 8 8.2h60c4.4 0 7.9-3.5 8-7.8 2-77.2 33-149.5 87.8-204.3 56.7-56.7 132-87.9 212.2-87.9s155.5 31.2 212.2 87.9C779 752.7 810 825 812 902.2c.1 4.4 3.6 7.8 8 7.8h60a8 8 0 0 0 8-8.2c-1-47.8-10.9-94.3-29.5-138.2zM512 534c-45.9 0-89.1-17.9-121.6-50.4S340 407.9 340 362c0-45.9 17.9-89.1 50.4-121.6S466.1 190 512 190s89.1 17.9 121.6 50.4S684 316.1 684 362c0 45.9-17.9 89.1-50.4 121.6S557.9 534 512 534z"></path> </g></svg>
+                <span class="truncate underline-animation">Minha Conta</span>
+                <svg class="flex h-4 w-4 flex flex-row" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                    <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                </svg>
+            </div>
+            <div class="max-h-0 overflow-hidden duration-300 group-focus:max-h-40">
+                <a class="flex items-center h-8 px-4 text-sm hover:bg-gray-200" href="/myaccount">
+                    <p class="underline-animation">Ver Perfil</p>
+                </a>
+                <a id="" class="flex items-center h-8 px-4 text-sm hover:bg-gray-200" href="">
+                    <p class="underline-animation">Sair da Conta</p>
+                </a>
+            </div>
+        </button>
+         @endauth
     </div>
   </div>
 </div>
