c30f49cf136585bacaf21b3c227dbc7f829ab0d4
angreen1623 <andreopaim@gmail.com>
2024-08-28 22:31:14 -0300

    botão do adicione novo anúncio

--- resources/views/meusartigos.blade.php
+++ resources/views/meusartigos.blade.php
@@ -66,7 +66,27 @@
                         @endforeach
                     @endif
 
-                    <button><a href="/announce">butão de criar artigo muito provisório</a></button>
+                    <div class="grid grid-cols-1 gap-4 justify-items-center">
+                            <div class="w-full max-w-48 mt-6 mb-9 sm:max-w-none">
+                                <a href="/announce">
+                                    <div class="flex flex-col sm:flex-row gap-3 bg-white rounded-3xl overflow-hidden items-center justify-start border-2 border-graytt-light shadow-tt transition ease-in-out delay-100 hover:-translate-y-1 hover:scale-105 duration-300">
+
+                                        <!-- Imagem e informações básicas -->
+                                        <div class="flex justify-center w-full sm:w-auto">
+                                            <div class="ml-3 mt-3 mb-3 overflow-hidden relative w-20 h-20 flex justify-center items-center">
+
+                                                <img class="rounded-xl w-5/6 h-5/6 object-cover object-center transition duration-50" loading="lazy" src="{{ asset('image/mais.svg') }}">
+
+                                            </div>
+                                        </div>
+                                        <div class="flex flex-col gap-2 py-2 w-full text-center sm:text-left">
+                                            <p class="truncate text-black text-xs sm:text-lg md:text-xl lg:text-2xl xl:text-3xl">Crie um novo anúncio</p>
+                                        </div>
+
+                                    </div>
+                                </a>
+                            </div>
+                        </div>
 
                 </div>
             </div>
